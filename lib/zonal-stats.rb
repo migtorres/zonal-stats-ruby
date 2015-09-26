f0bd0d05ba4a9e53bdbe65c223e1d1315ea51081
Miguel Torres <migtorres@gmail.com>
2015-09-26 23:14:12 +0100

    adds tests and module to basic opening of vector and raster files

--- lib/zonal-stats.rb
+++ lib/zonal-stats.rb
@@ -1,6 +1,38 @@
+require 'gdal'
 
 class ZonalStats
-  def calculate(raster: raster, vector: vector)
-
+  def read_raster raster_file: raster_file
+    raster = Gdal::Gdal.open(raster_file)
+    geotransform = geotransform_params(raster: raster)
   end
+
+  def read_vector vector_file: vector_file
+    vector = Gdal::Ogr.open(vector_file)
+    vector_layer = vector_layer(vector: vector)
+  end
+
+  def bbox_to_pixel_offsets(geotransform: geotransform, vector_extent: vector_extent)
+    origin_x = geotransform[0]
+    origin_y = geotransform[3]
+    pixel_width = geotransform[1]
+    pixel_height = geotransform[5]
+    x1 = ((vector_extent[0] - origin_x) / pixel_width).to_int
+    x2 = ((vector_extent[1] - origin_x) / pixel_width).to_int + 1
+
+    y1 = ((vector_extent[3] - origin_y) / pixel_height).to_int
+    y2 = ((vector_extent[2] - origin_y) / pixel_height).to_int + 1
+
+    xsize = x2 - x1
+    ysize = y2 - y1
+    { x1: x1, y1: y1, xsize: xsize, ysize: ysize }
+  end
+
+  def geotransform_params raster: raster
+    raster.get_geotransform
+  end
+
+  def vector_layer vector: vector
+    vector.get_layer(0)
+  end
+
 end
--- spec/zonal-stats_spec.rb
+++ spec/zonal-stats_spec.rb
@@ -1,13 +1,45 @@
 require 'spec_helper'
+require 'rspec/mocks'
 
 describe ZonalStats do
+  let(:zonal_stats) { ZonalStats.new}
 
-  it 'Expects reading two arguments' do
-    raster = 'raster.tif'
-    vector = 'vector.geojson'
+  context "#read_raster" do
 
-    zonal_stats = ZonalStats.new
-    zonal_stats.calculate(raster: raster, vector: vector)
+    it 'opens raster' do
+      geotransform = [-180, 0.005, 0.0, 90, 0.0, -0.005]
+      raster_file = 'raster_file.tif'
+      raster = double("raster")
+      raster.should_receive(:get_geotransform).and_return(geotransform)
+      #raster.should_receive(:get_raster_band).with(1)
+      Gdal::Gdal.should_receive(:open).with('raster_file.tif').and_return(raster)
+      zonal_stats.read_raster(raster_file: 'raster_file.tif')
+    end
+
+    it 'gets raster band'
+
+    it 'sets no data value'
+    it 'gets geo transform'
+  end
+
+  context "bounding box" do
+
+    it 'gets bounding box to pixel' do
+      geotransform = [-180, 0.005, 0.0, 90, 0.0, -0.005]
+      vector_extent = [0, 0, 50, 50]
+      bbox_to_pixel = zonal_stats.bbox_to_pixel_offsets(geotransform: geotransform, vector_extent: vector_extent)
+      bbox_to_pixel.should == { x1: 36000, y1: 8000, xsize: 1, ysize: 1 }
+    end
+  end
+
+  context "#read_vector" do
+    it 'opens vector' do
+      vector_file = 'vector_file.geojson'
+      vector = double("vector")
+      vector.should_receive(:get_layer).with(0)
+      Gdal::Ogr.should_receive(:open).with(vector_file).and_return(vector)
+      zonal_stats.read_vector(vector_file: vector_file)
+    end
 
   end
 end
